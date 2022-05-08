973613b3d0aba635bee5141e8969f9fe27412f1b
Arlette BELLO <adiath-arlette.bello@campusdulac.com>
2022-05-08 20:55:37 +0200

    Creéation de section 1 de la page d'acceuil avec animation du texte en Js

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+const textAnim = document.querySelector('.welcome');
+
+new Typewriter(textAnim, {})
+
+.typeString('Bienvenue sur NourShop <br/>')
+.typeString('Votre Boutique de présentation de tissus Africain')
+.pauseFor(300)
+.start();
+
+const textAnime = document.querySelector('.comme');
+new typeString(textAnime, {
+
+})
+.typeString('Avec NourShop Voyez la vie autrement ')
+.pauseFor(200)
+.start();
